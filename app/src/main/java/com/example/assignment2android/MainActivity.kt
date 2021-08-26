fb5f8898c1424657f2ed2744ca6235f44c10ad6e
Nandasejudith <judithnandase@gmail.com>
2021-08-26 18:51:26 +0300

    third commit

--- app/src/main/java/com/example/assignment2android/MainActivity.kt
+++ app/src/main/java/com/example/assignment2android/MainActivity.kt
@@ -2,8 +2,15 @@ package com.example.assignment2android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+    lateinit var etMobileWallet: EditText
+    lateinit var etOptions: EditText
+    lateinit var btnCash: Button
+    lateinit var btnCredit: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
