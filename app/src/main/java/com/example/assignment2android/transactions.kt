28907272a4bb0127d9b3fdccb3c0785c266af2d8
Nandasejudith <judithnandase@gmail.com>
2021-08-26 18:56:16 +0300

    transactions

---
+++ app/src/main/java/com/example/assignment2android/transactions.kt
@@ -0,0 +1,11 @@
+package com.example.assignment2android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class transactions : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_transactions)
+    }
+}
